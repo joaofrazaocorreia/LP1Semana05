6ad255ff24c0e11389e13f231fdc81ec314e70cd
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-30 02:12:51 +0100

    Format printed text to fit exercise assignments

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -22,12 +22,10 @@ namespace Tempo
             TimeSpan ts1 = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
 
-            string elapsedTime1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    ts1.Hours, ts1.Minutes, ts1.Seconds,
-                    ts1.Milliseconds / 10);
-            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    ts2.Hours, ts2.Minutes, ts2.Seconds,
-                    ts2.Milliseconds / 10);
+            string elapsedTime1 = String.Format("{0:00}.{1:00}",
+                    ts1.Seconds, ts1.Milliseconds / 10);
+            string elapsedTime2 = String.Format("{0:00}.{1:00}",
+                    ts2.Seconds, ts2.Milliseconds / 10);
 
             Console.WriteLine(elapsedTime1);
             Console.WriteLine(elapsedTime2);
