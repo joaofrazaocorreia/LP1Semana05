50006bfc06aefee7ccc84d11be8f79159f3cf01f
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-31 00:22:43 +0100

    Create project RandomDice, recieve a number and roll a dice that many times, then print the rolls and total sum

---
+++ RandomDice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randNum = new Random();
+
+            Console.Write("Insert number of rolls: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] rolls = new int [n];
+            int total = 0;
+
+            Console.WriteLine("\nYou rolled:");
+
+            for (int i = 0; i < n; i++)
+            {   
+                rolls[i] = randNum.Next(1, 7);
+                Console.Write(rolls[i]+"  ");
+                total += rolls[i];
+            }
+
+            Console.WriteLine("\nTotal: " + total);
+        }
+    }
+}
