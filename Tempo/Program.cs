6377a5e7065e744f8198d8fd3bcffa385eb4a866
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-31 13:40:34 +0100

    Fix time formatting

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -22,9 +22,9 @@ namespace Tempo
             TimeSpan ts1 = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
 
-            string elapsedTime1 = String.Format("{0:00}.{1:00}",
+            string elapsedTime1 = String.Format("{0:}.{1:00}",
                     ts1.Seconds, ts1.Milliseconds / 10);
-            string elapsedTime2 = String.Format("{0:00}.{1:00}",
+            string elapsedTime2 = String.Format("{0:}.{1:00}",
                     ts2.Seconds, ts2.Milliseconds / 10);
 
             Console.WriteLine(elapsedTime1);
