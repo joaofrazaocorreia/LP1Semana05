292a6bf44c45a99da55ad5b9b0f2281bb304915b
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-03-31 13:44:56 +0100

    Add project LowerUnion. Build a string with all given arguments in lowercase and sorted alphabetically

---
+++ LowerUnion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace LowerUnion
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array.Sort(args);
+
+            StringWriter output = new StringWriter();
+
+            if (args.Length == 0)
+            {
+                output.Write("sem qualquer tipo de argumento!");
+            }
+
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    output.Write(args[i].ToLower());
+                    
+                    if (i != args.Length - 1)
+                    {
+                        output.Write("_");
+                    }
+                }
+            }
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
